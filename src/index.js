f9303065b0f764edc47b226c083fff5605d95210
Yuriy Kuzin <yuriy.kuzin.pd@gmail.com>
2021-10-01 21:45:33 +0300

    updated index.js, added context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { DcContextProvider } from "./store/dc-context";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <DcContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </DcContextProvider>,
+  document.getElementById("root")
 );
