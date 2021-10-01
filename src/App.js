e24a45e0304b7cd8f0c85fb0a844284f3477f134
Yuriy Kuzin <yuriy.kuzin.pd@gmail.com>
2021-10-01 21:49:29 +0300

    updated app.js added routing to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { Route, Switch } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import PrivateRoute from "./components/PrivateRoute";
+import Canceled from "./pages/Canceled";
+import Confirmed from "./pages/Confirmed";
+import Pending from "./pages/Pending";
 
-export default function App() {
+
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Appointments/>
+        </Route>
+        <PrivateRoute path='/pending' component={Pending}/>
+        <PrivateRoute path='/confirmed' component={Confirmed}/>
+        <PrivateRoute path='/canceled' component={Canceled}/>
+      </Switch>
+    </Layout>
+  );
 }
 
+export default App;
