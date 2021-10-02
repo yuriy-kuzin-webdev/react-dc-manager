6eba1601858ff42d5c9ce3f34a09423a14b319a7
Yuriy Kuzin <yuriy.kuzin.pd@gmail.com>
2021-10-02 03:07:36 +0300

    added Canceled.js

--- src/pages/Canceled.js
+++ src/pages/Canceled.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Table from "react-bootstrap/Table";
+import DcContext from "../store/dc-context";
+import Appointment from "../components/Appointment";
 
 export default function Canceled() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const context = useContext(DcContext);
+  function getDentist(dentistId) {
+    return context.dentists.find((dentist) => dentist.id === dentistId);
+  }
+  function getClient(clientId) {
+    return context.clients.find((client) => client.id === clientId);
+  }
+  return (
+    <section>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Dentist</th>
+            <th>Date</th>
+            <th>Client</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {context.canceled.map((appointment) => (
+            <Appointment
+              key={appointment.id}
+              appointment={appointment}
+              dentist={getDentist(appointment.dentistId)}
+              client={getClient(appointment.clientId)}
+            />
+          ))}
+        </tbody>
+      </Table>
+    </section>
+  );
 }
